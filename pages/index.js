17798fc3af5d42beeb5895ac458e3e6569dcce5c
VogAnly <zy1367948142@foxmail.com>
2025-06-11 18:43:20 +0800

    更新 index.js

--- pages/index.js
+++ pages/index.js
@@ -4,7 +4,6 @@ import localFont from "next/font/local";
 import styles from "@/styles/Home.module.css";
 import "../tg/setgt.js";
 
-
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
   variable: "--font-geist-sans",

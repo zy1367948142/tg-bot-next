9b74afa3ba56ad9721a3c2a68ab1c746c5056790
karoly <karolyz526@gmail.com>
2025-06-12 00:12:33 +0800

    1

--- pages/index.js
+++ pages/index.js
@@ -2,7 +2,7 @@ import Head from "next/head";
 import Image from "next/image";
 import localFont from "next/font/local";
 import styles from "@/styles/Home.module.css";
-import "../tg/setgt.js";
+import "@/tg/setgt.js";
 
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
